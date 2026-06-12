335e47c1bb102dca7a263025aa7f3792d582ab54
Evan Hahn <me@evanhahn.com>
2026-06-11 19:38:08 -0500

    Replace Mocha with `node --test`

--- test/index.js
+++ test/index.js
@@ -2,23 +2,22 @@ var coinflip = require('..');
 
 var isBoolean = require('lodash.isboolean');
 var assert = require('assert');
+var test = require('node:test');
 
-describe('coinflip', function () {
-  it('always returns a boolean', function () {
-    for (var i = 0; i < 10000; i++) {
-      assert(isBoolean(coinflip()));
-    }
-  });
+test('coinflip always returns a boolean', function () {
+  for (var i = 0; i < 10000; i++) {
+    assert(isBoolean(coinflip()));
+  }
+});
 
-  it('eventually returns true', function () {
-    while (true) {
-      if (coinflip() === true) { break; }
-    }
-  });
+test('coinflip eventually returns true', function () {
+  while (true) {
+    if (coinflip() === true) { break; }
+  }
+});
 
-  it('eventually returns false', function () {
-    while (true) {
-      if (coinflip() === false) { break; }
-    }
-  });
+test('coinflip eventually returns false', function () {
+  while (true) {
+    if (coinflip() === false) { break; }
+  }
 });
