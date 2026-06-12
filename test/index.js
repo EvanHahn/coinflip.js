9d55303862904c20a2f0f4945367051dc3d1ef8c
Evan Hahn <me@evanhahn.com>
2026-06-11 19:45:08 -0500

    Tidy tests

--- test/index.js
+++ test/index.js
@@ -3,24 +3,20 @@ import coinflip from "../coinflip.js";
 import assert from "node:assert";
 import test from "node:test";
 
-test("coinflip always returns a boolean", function () {
-  for (var i = 0; i < 10000; i++) {
+test("coinflip always returns a boolean", () => {
+  for (let i = 0; i < 100; i++) {
     assert.equal(typeof coinflip(), "boolean");
   }
 });
 
-test("coinflip eventually returns true", function () {
+test("coinflip eventually returns true", () => {
   while (true) {
-    if (coinflip() === true) {
-      break;
-    }
+    if (coinflip() === true) break;
   }
 });
 
-test("coinflip eventually returns false", function () {
+test("coinflip eventually returns false", () => {
   while (true) {
-    if (coinflip() === false) {
-      break;
-    }
+    if (coinflip() === false) break;
   }
 });
